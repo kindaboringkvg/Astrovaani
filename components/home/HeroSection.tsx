f112e0193a9098b713c6b4f4724e8c27d9c3b4d2
kindaboringkvg <karam.veer00@gmail.com>
2025-06-03 00:55:02 +0530

    direct import for Moon (checking)

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -4,6 +4,7 @@ import { useEffect, useRef } from "react"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
+import Moon from './Moon.svg'
 
 export default function HeroSection() {
   const parallaxRef = useRef<HTMLDivElement>(null)
